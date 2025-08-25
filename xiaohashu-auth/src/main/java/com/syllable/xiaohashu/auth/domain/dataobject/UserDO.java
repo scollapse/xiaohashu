adb58fa2f185c58164160db815bef6242b8d19a8
syl <babaxiteyo@163.com>
2025-08-25 17:21:12 +0800

    feat:  引入 mybatis 代码生成器，重新生成 do、mapper、xml 文件

--- xiaohashu-auth/src/main/java/com/syllable/xiaohashu/auth/domain/dataobject/UserDO.java
+++ xiaohashu-auth/src/main/java/com/syllable/xiaohashu/auth/domain/dataobject/UserDO.java
@@ -6,13 +6,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class UserDO {
-
     private Long id;
 
     private String username;
@@ -20,5 +20,5 @@ public class UserDO {
     private LocalDateTime createTime;
 
     private LocalDateTime updateTime;
-}
 
+}
