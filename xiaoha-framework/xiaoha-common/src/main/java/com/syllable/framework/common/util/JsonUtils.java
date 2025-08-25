18bb19d105d55320831bd005c49bab5df9d435e8
syl <babaxiteyo@163.com>
2025-08-25 14:19:03 +0800

    feat: - 创建封装日志 starter并进行测试

---
+++ xiaoha-framework/xiaoha-common/src/main/java/com/syllable/framework/common/util/JsonUtils.java
@@ -0,0 +1,51 @@
+package com.syllable.framework.common.util;
+
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.syllable.framework.common.constant.DateConstants;
+import lombok.SneakyThrows;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @description: JsonUtils
+ * @author: syl
+ * @create: 2025-08-25 14:09
+ **/
+public class JsonUtils {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    static {
+        OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        OBJECT_MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+
+        // JavaTimeModule 用于指定序列化和反序列化规则
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+
+        // 支持 LocalDateTime
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DateConstants.Y_M_D_H_M_S_FORMAT)));
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DateConstants.Y_M_D_H_M_S_FORMAT)));
+
+        OBJECT_MAPPER.registerModules(javaTimeModule); // 解决 LocalDateTime 的序列化问题
+    }
+
+
+
+    /**
+     *  将对象转换为 JSON 字符串
+     *  @SneakyThrows: 这是 Lombok 提供的一个注解，用于简化异常处理。它会将被标注的方法中的受检异常转换为不受检异常，使得代码看起来更加简洁。
+     * @param obj
+     * @return
+     */
+    @SneakyThrows
+    public static String toJsonString(Object obj) {
+        return OBJECT_MAPPER.writeValueAsString(obj);
+    }
+}
