0f6ba4c22a03fcd05145c07f1e451f80e4828794
syl <babaxiteyo@163.com>
2025-08-25 10:32:47 +0800

    init

---
+++ xiaohashu-auth/src/main/java/com/syllable/xiaohashu/auth/XiaohashuAuthApplication.java
@@ -0,0 +1,13 @@
+package com.syllable.xiaohashu.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaohashuAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohashuAuthApplication.class, args);
+    }
+
+}
---
+++ xiaohashu-auth/src/test/java/com/syllable/xiaohashu/auth/XiaohashuAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syllable.xiaohashu.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaohashuAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
