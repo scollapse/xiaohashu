70101fa915995616760ef9253a2fce338e5a4202
syl <babaxiteyo@163.com>
2025-08-26 11:32:50 +0800

    feature :  增加全局异常捕获，接口参数校验

--- xiaohashu-auth/src/main/java/com/syllable/xiaohashu/auth/controller/TestController.java
+++ xiaohashu-auth/src/main/java/com/syllable/xiaohashu/auth/controller/TestController.java
@@ -3,6 +3,7 @@ package com.syllable.xiaohashu.auth.controller;
 
 import com.syllable.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.syllable.framework.common.response.Response;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,7 +28,8 @@ public class TestController {
 
     @PostMapping("/test2")
     @ApiOperationLog(description = "测试接口2")
-    public Response<User> test2(@RequestBody User user) {
+    public Response<User> test2(@RequestBody @Validated User user) {
       return Response.success(user);
     }
+
 }
