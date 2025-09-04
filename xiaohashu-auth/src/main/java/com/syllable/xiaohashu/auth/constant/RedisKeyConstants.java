23caa9ee61c29786ba17da6b5ba4faf218051a23
syl <babaxiteyo@163.com>
2025-09-04 18:19:08 +0800

    feat: 增加角色权限
    1.增加 MyBatis 配置以支持下划线到驼峰的映射
    2.并添加了查询启用权限和角色的方法
    3.以及在服务启动时将角色权限数据同步到 Redis 的功能。

--- xiaohashu-auth/src/main/java/com/syllable/xiaohashu/auth/constant/RedisKeyConstants.java
+++ xiaohashu-auth/src/main/java/com/syllable/xiaohashu/auth/constant/RedisKeyConstants.java
@@ -16,12 +16,18 @@ public class RedisKeyConstants {
     /**
      * 小哈书全局 ID 生成器 KEY
      */
-    public static final String XIAOHASHU_ID_GENERATOR_KEY = "xiaohashu_id_generator";
+    public static final String XIAOHASHU_ID_GENERATOR_KEY = "xiaohashu.id.generator";
 
     /**
      * 用户角色数据 KEY 前缀
      */
     private static final String USER_ROLES_KEY_PREFIX = "user:roles:";
+
+    /**
+     * 角色对应的权限集合 KEY 前缀
+     */
+    private static final String ROLE_PERMISSIONS_KEY_PREFIX = "role:permissions:";
+
 
 
     /**
@@ -41,5 +47,14 @@ public class RedisKeyConstants {
      */
     public static String buildUserRoleKey(String phone) {
         return USER_ROLES_KEY_PREFIX + phone;
+    }
+
+    /**
+     * 构建角色对应的权限集合 KEY
+     * @param roleId
+     * @return
+     */
+    public static String buildRolePermissionsKey(Long roleId) {
+        return ROLE_PERMISSIONS_KEY_PREFIX + roleId;
     }
 }
--- xiaohashu-auth/src/main/java/com/syllable/xiaohashu/auth/domain/mapper/RolePermissionDOMapper.java
+++ xiaohashu-auth/src/main/java/com/syllable/xiaohashu/auth/domain/mapper/RolePermissionDOMapper.java
@@ -1,6 +1,9 @@
 package com.syllable.xiaohashu.auth.domain.mapper;
 
 import com.syllable.xiaohashu.auth.domain.dataobject.RolePermissionDO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface RolePermissionDOMapper {
     int deleteByPrimaryKey(Long id);
@@ -14,4 +17,13 @@ public interface RolePermissionDOMapper {
     int updateByPrimaryKeySelective(RolePermissionDO record);
 
     int updateByPrimaryKey(RolePermissionDO record);
+
+    /**
+     * 根据角色 ID 集合批量查询
+     *
+     * @param roleIds
+     * @return
+     */
+    List<RolePermissionDO> selectByRoleIds(@Param("roleIds") List<Long> roleIds);
+
 }
