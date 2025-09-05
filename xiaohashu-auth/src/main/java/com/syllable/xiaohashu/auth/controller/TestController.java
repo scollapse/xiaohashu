41394678620f24f473e4358f636e7ca000cdc2dd
syl <babaxiteyo@163.com>
2025-09-05 15:53:26 +0800

    feat: 配置 nacos 作为配置中心、服务发现，并进行测试

--- xiaohashu-auth/src/main/java/com/syllable/xiaohashu/auth/controller/TestController.java
+++ xiaohashu-auth/src/main/java/com/syllable/xiaohashu/auth/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.syllable.xiaohashu.auth.controller;
 
 import com.alibaba.nacos.api.config.annotation.NacosValue;
+import com.syllable.xiaohashu.auth.alarm.AlarmInterface;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,17 @@ public class TestController {
     @GetMapping("/test")
     public String test() {
         return "当前限流阈值为: " + limit;
+    }
+
+
+    @Resource
+    private AlarmInterface alarm;
+
+
+    @GetMapping("/alarm")
+    public String sendAlarm() {
+        alarm.send("系统出错啦，犬小哈这个月绩效没了，速度上线解决问题！");
+        return "alarm success";
     }
 
 }
