7dc49a269635c267e3fb98a168af32fdd82fd09b
syl <babaxiteyo@163.com>
2025-09-08 13:53:28 +0800

    feat:  实现 ID 透传到下游服务

--- xiaohashu-auth/src/main/java/com/syllable/xiaohashu/auth/controller/UserController.java
+++ xiaohashu-auth/src/main/java/com/syllable/xiaohashu/auth/controller/UserController.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,9 +35,10 @@ public class UserController {
 
     @PostMapping("/logout")
     @ApiOperationLog(description = "账号登出")
-    public Response<?> logout() {
+    public Response<?> logout(@RequestHeader("userId") String userId) {
+        // todo 账号登录逻辑待实现
 
-        // todo 账号退出登录逻辑待实现
+        log.info("==> 网关透传过来的用户 ID: {}", userId);
 
         return Response.success();
     }
